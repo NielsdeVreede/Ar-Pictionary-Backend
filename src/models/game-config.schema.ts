03846135ec65415ed7b0e53d4792d967a05b370e
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2021-12-21 12:25:22 +0100

    Update gameConfig data

--- src/models/game-config.schema.ts
+++ src/models/game-config.schema.ts
@@ -1,6 +1,11 @@
 export default class GameConfig {
+  gameID: number;
   word: string;
   timeLimitInSeconds: number;
+  timeRemaining: number;
   started: boolean;
   guesses: string[];
+  guessableWord: string;
+  currentPlayerTurn: number
+  players: string[];
 }
